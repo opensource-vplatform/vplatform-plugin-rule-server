c7c87c9c3ae02e4537a20aafa0e7002f5fa5db3c
jiqj <316719744@qq.com>
2021-06-18 15:11:14 +0800

    改造 DeleteConditionRelationData 规则

--- Serverrule_DeleteConditionRelationData/src/main/java/com/toone/itop/formula/rule/inte/DeleteConditionRelationData.java
+++ Serverrule_DeleteConditionRelationData/src/main/java/com/toone/itop/formula/rule/inte/DeleteConditionRelationData.java
@@ -25,85 +25,96 @@ import com.yindangu.v3.platform.plugin.util.VdsUtils;
  */
 public class DeleteConditionRelationData implements IRule  { // extends AbstractRule4Tree {
 	
-		private static final Logger log = LoggerFactory.getLogger(DeleteConditionRelationData.class); 
-		public static final String D_RULE_NAME = "删除数据库中的记录";
-		public static final String D_RULE_CODE = "DeleteConditionRelationData";
-		public static final String D_RULE_DESC = "删除数据库中选中表的记录，支持条件筛选。\r\n" + 
-				"方法名：" + D_RULE_CODE;
-		private static final String D_dtChileMaps= "dtChileMaps";
-		private static final String D_treeStruct = "treeStruct";
+	private static final Logger log = LoggerFactory.getLogger(DeleteConditionRelationData.class); 
+	public static final String D_RULE_NAME = "删除数据库中的记录";
+	public static final String D_RULE_CODE = "DeleteConditionRelationData";
+	public static final String D_RULE_DESC = "删除数据库中选中表的记录，支持条件筛选。\r\n" + 
+			"方法名：" + D_RULE_CODE;
+	private static final String D_dtChileMaps= "dtChileMaps";
+	private static final String D_treeStruct = "treeStruct";
 			
 			
     @SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public IRuleOutputVo evaluate(IRuleContext context) {
+    	log.info("新接口。。。");
         //Map<String, Object> inParams = (Map<String, Object>) context.getRuleConfig().getConfigParams();
         List<Map> dtChileMaps = (List<Map>) context.getPlatformInput(D_dtChileMaps);
         if (VdsUtils.collection.isEmpty(dtChileMaps)) {
             return context.newOutputVo();
         }
-
-        //DAS das = IMetaDataFactory.getService().das();
-        IDAS das =  VDS.getIntance().getDas();
-        
         //List<Map> treeStructList = inParams.get("treeStruct") != null ? (List<Map>) inParams.get("treeStruct") : null; // 树形结构
         List<Map> treeStructList =  (List<Map>) context.getPlatformInput(D_treeStruct);
-        boolean isTree = !(VdsUtils.collection.isEmpty(treeStructList));
-        if (isTree) {
-            //DASRuntimeContextFactory.getService().addTreeStructMaps(treeStructList);
-        	das.getContext().addTreeStructMaps(treeStructList);
-        }
-        IQueryParse vparse= VDS.getIntance().getVSqlParse(); 
-        for (Map dtChileMap : dtChileMaps) {
-            String tableName = (String) dtChileMap.get("tableName");
-            List<Map> conditions = (List<Map>) dtChileMap.get("dsWhere");
-            /*SQLBuf sql = QueryConditionUtil
-                    .parseConditionsNotSupportRuleTemplate(conditions);
-            String condSql = sql.getSQL();
-            Map condParams = sql.getParams();
-            */
-    		@SuppressWarnings("deprecation")
-    		ISQLBuf sql = vparse.parseConditionsJson(conditions);
-    		String condSql = sql.getSQL();
-            Map condParams = sql.getParams();
-            // 拼查询语句
-            String countFieldAlias = "countField";
-            StringBuilder sbSelect = new StringBuilder(" select count(1) as ");
-            sbSelect.append(countFieldAlias);
-            sbSelect.append(" from ").append(tableName).append(" ");
-            String conditionsql = null;
-            
-            //if (!StringUtils.isBlank(condSql)) {
-            if (!VdsUtils.string.isEmpty(condSql)) {
-                conditionsql = "where ";
-                if (condSql.startsWith("  and  ") || condSql.startsWith("  or  ")) {
-                    conditionsql = conditionsql + " 1 = 1 ";
-                }
-                conditionsql = conditionsql + condSql;
-                sbSelect.append(conditionsql);
-            }
-
-            // 查数据
-            //DataView dataview = das.findWithNoFilter(sbSelect.toString(), condParams);
-            IDataView dataview = das.findWithNoFilter(sbSelect.toString(), condParams);
-            List<IDataObject> datas = dataview.select();
-            
-            long totalValue =toLong(datas.get(0).get(countFieldAlias)); 
-            // 如果有要删的数据，才执行删除
-            if (totalValue > 0) {
-                if (isTree) {// 树形结构删除
-                    deleteTreeDatas(das, tableName, conditionsql, condParams);
-                } else { // 普通表删除
-                    deleteDatas(das,tableName, conditionsql, condParams);
-                }
-            } 
-        }
+        evaluateSub(dtChileMaps, treeStructList);
         return context.newOutputVo().put(Boolean.TRUE);
     }
-    private long toLong(Object total) {
-    	long v =-1;
+    private void evaluateSub(List<Map> dtChileMaps,List<Map> treeStructList) { 
+        //DAS das = IMetaDataFactory.getService().das();
+        IDAS das =  VDS.getIntance().getDas(); 
+        boolean isTree  = (treeStructList!=null && treeStructList.size()>0);
+        try {
+	        if (isTree) {
+	            //DASRuntimeContextFactory.getService().addTreeStructMaps(treeStructList);
+	        	das.getContext().init();
+	        	das.getContext().addTreeStructMaps(treeStructList);
+	        }
+	        IQueryParse vparse= VDS.getIntance().getVSqlParse(); 
+	        for (Map dtChileMap : dtChileMaps) {
+	            String tableName = (String) dtChileMap.get("tableName");
+	            List<Map> conditions = (List<Map>) dtChileMap.get("dsWhere");
+	            /*SQLBuf sql = QueryConditionUtil
+	                    .parseConditionsNotSupportRuleTemplate(conditions);
+	            String condSql = sql.getSQL();
+	            Map condParams = sql.getParams();
+	            */
+	    		@SuppressWarnings("deprecation")
+	    		ISQLBuf sql = vparse.parseConditionsJson(conditions);
+	    		String condSql = sql.getSQL();
+	            Map condParams = sql.getParams();
+	            // 拼查询语句
+	            String countFieldAlias = "countField";
+	            StringBuilder sbSelect = new StringBuilder(" select count(1) as ");
+	            sbSelect.append(countFieldAlias);
+	            sbSelect.append(" from ").append(tableName).append(" ");
+	            String conditionsql = null;
+	            
+	            //if (!StringUtils.isBlank(condSql)) {
+	            if (!VdsUtils.string.isEmpty(condSql)) {
+	                conditionsql = "where ";
+	                if (condSql.startsWith("  and  ") || condSql.startsWith("  or  ")) {
+	                    conditionsql = conditionsql + " 1 = 1 ";
+	                }
+	                conditionsql = conditionsql + condSql;
+	                sbSelect.append(conditionsql);
+	            }
+	
+	            // 查数据
+	            //DataView dataview = das.findWithNoFilter(sbSelect.toString(), condParams);
+	            IDataView dataview = das.findWithNoFilter(sbSelect.toString(), condParams);
+	            List<IDataObject> datas = dataview.select();
+	            
+	            int totalValue =toLong(datas.get(0).get(countFieldAlias)); 
+	            // 如果有要删的数据，才执行删除
+	            if (totalValue > 0) {
+	                if (isTree) {// 树形结构删除
+	                    deleteTreeDatas(das, tableName, conditionsql, condParams);
+	                } else { // 普通表删除
+	                    deleteDatas(das,tableName, conditionsql, condParams);
+	                }
+	            } 
+	        }
+    	}
+    	finally {
+    		 if (isTree) { 
+ 	        	das.getContext().clear();
+ 	        }
+    	}
+    }
+ 
+    private int toLong(Object total) {
+    	int v =-1;
     	if (total != null && total instanceof Number) {
-    		v = ((Number) total).longValue(); 
+    		v = ((Number) total).intValue(); 
     	}
     	return v;
     }
