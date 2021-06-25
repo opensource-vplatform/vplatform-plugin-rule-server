bef9272b7a8e95a655c07f907ceabfad829ba52f
jiqj <316719744@qq.com>
2021-06-25 08:56:09 +0800

    保存实体到数据库

--- mvn-rules.bat
+++ mvn-rules.bat
@@ -4,12 +4,14 @@ chcp 65001
 set setting=E:\works\toone-maven-settings.xml
 echo ==================maven 仓库设置(%setting%)====================
 rd /s /Q target
+del /Q *.log
 mkdir target 
 
 echo =================批量打包=================
 for %%i in (
     Serverrule_EntityRecordRecycling
 	Serverrule_DeleteConditionRelationData
+	Serverrule_UpdateRecord
 ) do (
 	echo "mvn clean package -f %%i\pom.xml --settings %setting%" 
 	TIMEOUT /T 1
@@ -19,4 +21,5 @@ for %%i in (
    
 del target\*-javadoc.jar
 del target\*-sources.jar
-pause
+echo "**********打包结束**********"
+TIMEOUT /T 10
