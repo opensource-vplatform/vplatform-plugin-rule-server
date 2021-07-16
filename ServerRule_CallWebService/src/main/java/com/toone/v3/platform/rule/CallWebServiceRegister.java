fbcdc6b017960289e355789cca2ee15eb86b748e
ahxugang <xg_xugang@163.com>
2021-07-16 14:07:43 +0800

    增加规则：CallWebService

---
+++ ServerRule_CallWebService/src/main/java/com/toone/v3/platform/rule/CallWebServiceRegister.java
@@ -0,0 +1,60 @@
+package com.toone.v3.platform.rule;
+
+import com.yindangu.v3.plugin.vds.reg.api.IRegisterPlugin;
+import com.yindangu.v3.plugin.vds.reg.api.builder.IRuleBuilder;
+import com.yindangu.v3.plugin.vds.reg.api.model.IComponentProfileVo;
+import com.yindangu.v3.plugin.vds.reg.api.model.IPluginProfileVo;
+import com.yindangu.v3.plugin.vds.reg.common.RegVds;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @Author xugang
+ * @Date 2021/7/14 16:38
+ */
+public class CallWebServiceRegister implements IRegisterPlugin {
+
+    /**
+     * 插件作者
+     */
+    public final static String D_Author = "同望科技";
+    /**
+     * 组织标识
+     */
+    public final static String D_GroupId = "com.toone.v3.platform";
+    public final static String D_RULE_CODE = "CallWebService";
+    public final static String D_RULE_NAME = "调用WebService";
+    public final static String D_RULE_DESC = "开发系统，通过URL调用其他V平台系统WEBService或通过URL调用第三方WEBService\r\n" +
+            "方法名：CallWebService";
+    public final static String D_COMPONENT = "ServerRule_CallWebService";
+
+    @Override
+    public IComponentProfileVo getComponentProfile() {
+        return RegVds.getPlugin()
+                .getComponentProfile()
+                .setGroupId(D_GroupId)
+                .setCode(D_COMPONENT)
+                .setVersion("3.10.0")
+                .build();
+    }
+
+    @Override
+    public List<IPluginProfileVo> getPluginProfile() {
+        IPluginProfileVo ruleProfile = getRuleProfile();
+
+        return Collections.singletonList(ruleProfile);
+    }
+
+    private IPluginProfileVo getRuleProfile() {
+        IRuleBuilder ruleBuilder = RegVds.getPlugin().getRulePlugin();
+        ruleBuilder.setAuthor(D_Author)
+                .setCode(D_RULE_CODE)
+                .setDesc(D_RULE_DESC)
+                .setName(D_RULE_NAME)
+                .setEntry(CallWebService.class)
+        ;
+
+        return ruleBuilder.build();
+    }
+}
