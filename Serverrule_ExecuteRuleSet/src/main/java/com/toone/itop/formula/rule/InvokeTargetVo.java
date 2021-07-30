cd12af2aca74f89ca88442c007808f882faa8d16
jiqj <316719744@qq.com>
2021-07-30 11:05:05 +0800

    增加ExecuteRuleSet

---
+++ Serverrule_ExecuteRuleSet/src/main/java/com/toone/itop/formula/rule/InvokeTargetVo.java
@@ -0,0 +1,45 @@
+package com.toone.itop.formula.rule;
+
+public class InvokeTargetVo {
+	//private String executeType;
+	private String remoteUrl;
+	private String ruleSetCode;
+	private String componentCode;
+	private boolean parallelism;
+	/*
+	public String getExecuteType() {
+		return executeType;
+	}
+	public InvokeTargetVo setExecuteType(String executeType) {
+		this.executeType = executeType;
+		return this;
+	}*/
+	public String getRemoteUrl() {
+		return remoteUrl;
+	}
+	public InvokeTargetVo setRemoteUrl(String remoteUrl) {
+		this.remoteUrl = remoteUrl;
+		return this;
+	}
+	public String getRuleSetCode() {
+		return ruleSetCode;
+	}
+	public InvokeTargetVo setRuleSetCodes(Object ruleSetCode) {
+		this.ruleSetCode = (String)ruleSetCode;
+		return this;
+	}
+	public String getComponentCode() {
+		return componentCode;
+	}
+	public InvokeTargetVo setComponentCodes(Object componentCode) {
+		this.componentCode = (String)componentCode;
+		return this;
+	}
+	public boolean isParallelism() {
+		return parallelism;
+	}
+	public void setParallelisms(boolean parallelism) {
+		this.parallelism = parallelism;
+	}
+	
+}
